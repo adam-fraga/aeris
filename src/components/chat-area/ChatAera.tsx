e02e173c5db98b441fec3c42d0325a1d547cbf57
ad-mindset <contact@ad-mindset.com>
2023-04-14 16:51:12 +0200

    Refactorise components tree add Readme for dynamic content and rename few components

--- src/components/chat-area/ChatAera.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, PropsWithChildren } from "react";
-import { Logos } from "../../assets/images/svg/Logos";
-import "./ChatArea.css";
-
-interface ChatAreaProps extends PropsWithChildren { }
-
-export const ChatAera: FC<ChatAreaProps> = () => {
-  return <div id='chatArea' className=''></div>;
-};
